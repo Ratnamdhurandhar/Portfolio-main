8fdbeb60c6fba5400c338f886f071b485d28672e
ratnamdhurandhar <ratnamdhurandhar@gmail.com>
2024-01-31 15:27:36 +0530

    portfolio-main

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".typing",{
+    strings:["Full-Stack Developer.","Frontend Developer.","Web Developer.","Web Designer"],
+  typeSpeed:50,
+  backSpeed:60,
+  
+  loop:true
+  });
