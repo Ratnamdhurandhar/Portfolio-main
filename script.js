b5c804305e552cc0f095fc3a71d2613f205d8060
ratnamdhurandhar <ratnamdhurandhar@gmail.com>
2024-02-01 07:57:11 +0530

    form

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ var typed=new Typed(".typing",{
   
   loop:true
   });
+
+
+
+ //==============================================//
+ document.addEventListener('DOMContentLoaded', function() {
+  var form = document.getElementById('contactForm');
+
+  form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent default form submission behavior
+
+      // Clear form fields
+      document.getElementById('name').value = '';
+      document.getElementById('email').value = '';
+      document.getElementById('subject').value = '';
+      document.getElementById('message').value = '';
+  });
+});
